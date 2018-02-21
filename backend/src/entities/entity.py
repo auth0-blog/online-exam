88324d0a3e3be16a18e03f6f8ccded7ca71ee9b9
Bruno Krebs <bruno.krebs@auth0.com>
2018-02-21 02:11:31 -0300

    moving from Date to DateTime;

--- backend/src/entities/entity.py
+++ backend/src/entities/entity.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
-from datetime import date
-from sqlalchemy import create_engine, Column, String, Integer, Date
+from datetime import datetime
+from sqlalchemy import create_engine, Column, String, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -17,11 +17,11 @@ Base = declarative_base()
 
 class Entity():
     id = Column(Integer, primary_key=True)
-    created_at = Column(Date)
-    updated_at = Column(Date)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
     last_updated_by = Column(String)
 
     def __init__(self, created_by):
-        self.created_at = date.today()
-        self.updated_at = date.today()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         self.last_updated_by = created_by
